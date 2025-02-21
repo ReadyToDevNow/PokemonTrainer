983f32be53f72f3bc832f06155eeb4dccd36f832
ReadyToDevNow <learntolearn0@gmail.com>
2025-02-21 18:06:49 +0100

    feat() Create App

---
+++ src/components/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <span class="font-bold text-4xl flex flex-col justify-center items-center">
+      Bienvenue jeune dresseur
+    </span>
+    <span class="text-xl">Commences ton aventure</span>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
